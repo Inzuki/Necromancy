f6ef7252a1f2ee2057aeb989be7539299897960a
Inzuki <teh_inzuki@hotmail.com>
2014-10-12 17:01:00 -0500

    Pathfinding work
    + Added operations to check and make sure it did not go out of bounds
    - Fixed problem where the check would wrap around the map
    
    Near complete, still a little shoddy but might leave for now.

--- test/Maps.h
+++ test/Maps.h
@@ -5,16 +5,30 @@
 
 #include "Map.h"
 
-std::vector<mapInfo> map01_init(){
+/* a blank map
+
 	int map[8][13] = {
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-		{ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-		{ 0, 2, 0, 1, 0, 3, 0, 0, 0, 0, 0, 0, 0 },
-		{ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+	};
+*/
+
+std::vector<mapInfo> map01_init(){
+	int map[8][13] = {
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+		{ 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+		{ 0, 2, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0 },
+		{ 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0 },
+		{ 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 3 }
 	};
 
 	std::vector<mapInfo> mapStruct;
@@ -41,7 +55,7 @@ void setUpMap(std::vector<mapInfo> &map, std::vector<mapInfoPF> &mapPF){ // keep
 		mapPFPF.type = map[i].type;
 		mapPFPF.G = 0;
 		mapPFPF.H = 0;
-		mapPFPF.F = mapPFPF.G + mapPFPF.H;
+		mapPFPF.F = 0;
 		mapPFPF.checked = false;
 		mapPFPF.path = false;
 		mapPF.push_back(mapPFPF);
@@ -50,15 +64,12 @@ void setUpMap(std::vector<mapInfo> &map, std::vector<mapInfoPF> &mapPF){ // keep
 
 void doPathFind(std::vector<mapInfoPF> &mapPF){
 	int hTargetX = -1, hTargetY = -1,
-		currentX = -2, currentY = -2,
 		currentI = -1;
 
 	for(std::vector<mapInfoPF>::size_type i = 0; i < mapPF.size(); i++){
-		if(mapPF[i].type == 2){ // get start's coordinates
-			currentX = mapPF[i].x;
-			currentY = mapPF[i].y;
+		if(mapPF[i].type == 2) // get start's coordinates
 			currentI = i;
-		}else if(mapPF[i].type == 3){ // get finish's coordinates
+		else if(mapPF[i].type == 3){ // get finish's coordinates
 			hTargetX = mapPF[i].x;
 			hTargetY = mapPF[i].y;
 		}
@@ -74,36 +85,49 @@ void doPathFind(std::vector<mapInfoPF> &mapPF){
 
 	bool done = false;
 
-	while(!done){
+	//while(!done){
 		int checkAround[8] = { -1, 1, -13, 13, -14, -12, 12, 14 };
 		// respectively: check left, right, above, below, top-left, top-right, bottom-left, bottom-right
-		
-		printf("Current Space: ( %i, %i )\nTarget Space: ( %i, %i )\n\n", currentX, currentY, hTargetX, hTargetY);
 
+		for(int a = 0; a < 5; a++){
 		for(int i = 0; i < 8; i++){
-			if((mapPF[currentI + checkAround[i]].type == 0) && (mapPF[currentI + checkAround[i]].path == false)){
-				if(mapPF[currentI + checkAround[i]].H == 10)
-					done = true;
+			// don't go out of bounds
+			if((currentI + checkAround[i]) > 0 && (currentI + checkAround[i]) < 104){
+				// check if the space isn't a wall AND check if the space is not already on the path list
+				if((mapPF[currentI + checkAround[i]].type != 1) && (mapPF[currentI + checkAround[i]].path == false)){
+					// make sure the check doesn't wrap around the map
+					if(mapPF[currentI].x == 0 && mapPF[currentI + checkAround[i]].x == 12){
+						// do fuck all
+					}else {
+						// check if the neighbor is is the finish
+						if(mapPF[currentI + checkAround[i]].type == 3){
+							done = true;
+						}else {
+							printf("%i\n", mapPF[currentI].G);
 
-				if(i >= 0 && i < 4)
-					mapPF[currentI + checkAround[i]].G = mapPF[currentI].G + 10;
-				else
-					mapPF[currentI + checkAround[i]].G = mapPF[currentI].G + 14;
+							if(i > 3 && i < 8) // if the space is in the X (the diagonals)
+								mapPF[currentI + checkAround[i]].G = mapPF[currentI].G + 14;
+							else // if the space is in the cross
+								mapPF[currentI + checkAround[i]].G = mapPF[currentI].G + 10;
 
-				mapPF[currentI + checkAround[i]].F = mapPF[currentI + checkAround[i]].G + mapPF[currentI + checkAround[i]].H;
-				mapPF[currentI + checkAround[i]].checked = true;
+							mapPF[currentI + checkAround[i]].F = mapPF[currentI + checkAround[i]].G + mapPF[currentI + checkAround[i]].H;
+						
+							// check if the current F score is the lowest out of all the neighbors
+							if((mapPF[currentI + checkAround[i]].F) < currentHighF){
+								currentHighF = mapPF[currentI + checkAround[i]].F;
+								nextI = currentI + checkAround[i];
+							}
 
-				if((mapPF[currentI + checkAround[i]].F < currentHighF)){
-					currentHighF = mapPF[currentI + checkAround[i]].F;
-					nextI = currentI + checkAround[i];
+							mapPF[currentI + checkAround[i]].checked = true;
+						}
+					}
 				}
 			}
 		}
+		//}
 
 		currentI = nextI;
 		mapPF[currentI].path = true;
-		currentX = mapPF[currentI].x;
-		currentY = mapPF[currentI].y;
 		currentHighF = 666;
 	}
 }
